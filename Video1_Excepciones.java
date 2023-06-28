ea31c4c05cacbd6e87f89448c030a5996fc4ccc9
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2023-06-28 15:31:01 -0300

    Add files via upload

---
+++ Video1_Excepciones.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USURIO
+ */
+public class Video1_Excepciones {
+
+    public static void main(String[] args) {
+        ////////////////////////////////////////////
+        /////////////// Excepciones ////////////////
+        ////////////////////////////////////////////
+
+//        int valor1 = Integer.ValueOF("a");
+        ///////////////////////////////////////////////
+        ///////CAPTURAR EXECPCIONES INDICIDUALES///////
+        ///////////////////////////////////////////////
+//        int valor2 =5;
+//        System.out.println("El numero entero se inicializo en: "+ valor2);
+//        
+//        try {
+//            
+//            valor2 = Integer.parseInt("a");
+//            System.out.println("El numoero incluido es: "+ valor2);
+//        }catch(NumberFormatException e){
+//            System.out.println("El valor indicado no es un numero");
+//        }
+//        
+        ///////////////////////////////////////////////
+        /////////CAPTURAR TODAS LAS EXCEPCIONES////////
+        ///////////////////////////////////////////////
+//        int valor2=5;
+//        System.out.println("El numero entero se inicializo en: "+ valor2);
+//        try {
+//            
+//            int valor2 = Integer.parseInt("10");
+//            System.out.println("El numoero incluido es: "+ valor2);
+//        }catch(Exception e){
+//            System.out.println("El valor indicado no es un numero");
+//        }
+        ///////////////////////////////////////////////
+        //////////////////BLOQUE FINALLY///////////////
+        ///////////////////////////////////////////////
+//        int valor2 = 5;
+//        System.out.println("El numero entero se inicializo en: " + valor2);
+//
+//        try {
+//
+//            valor2 = Integer.parseInt("10");
+//            System.out.println("El numero incluido es: " + valor2);
+//        } catch (NumberFormatException e) {
+//            System.out.println("El valor indicado no es un numero");
+//
+//        } catch (Exception e) {
+//            System.out.println("El valor indicado no es un numero");
+//        }finally{
+//            int valor = 7;
+//            System.out.println("El valor indicado es: "+ valor);
+//        }
+
+    }
+}
